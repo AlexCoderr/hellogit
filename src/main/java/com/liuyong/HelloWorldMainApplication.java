79545f91e80305b52c487821f4f60398cc03c6b8
Administrator <213@qq.com>
2020-04-25 10:59:19 +0800

    ok

--- src/main/java/com/liuyong/HelloWorldMainApplication.java
+++ src/main/java/com/liuyong/HelloWorldMainApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldMainApplication.class, args);
-        // edit
     }
 }
