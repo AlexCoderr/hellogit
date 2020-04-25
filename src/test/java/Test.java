6d26634177f121fe67f29844518ef185600b9038
Administrator <213@qq.com>
2020-04-25 10:35:11 +0800

    init

---
+++ src/test/java/Test.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+
+@Configuration
+
+public class Test {
+}
